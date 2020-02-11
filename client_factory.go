703d9fcd201a9b9dc5f29afcc93571c23c40650b
Andrey Bukatov <andrey.bukatov@gmail.com>
2020-02-11 18:48:33 +0200

    Add doc for client factory

--- client_factory.go
+++ client_factory.go
@@ -4,20 +4,24 @@ import (
 	"net/http"
 )
 
+// WebsocketConnectionUpgrader upgrades HTTP connection to the WebSocket connection.
 type WebsocketConnectionUpgrader interface {
 	Upgrade(w http.ResponseWriter, r *http.Request) (WebsocketConnection, error)
 }
 
+// WebsocketConnection represents a WebSocket connection.
 type WebsocketConnection interface {
 	Read() (Message, error)
 	Write(message Message) error
 	Close() error
 }
 
+// UUIDGenerator generates UUID v4.
 type UUIDGenerator interface {
 	GenerateV4() UUID
 }
 
+// ClientFactory is responsible for creating a client.
 type ClientFactory struct {
 	options       ClientOptions
 	uuidGenerator UUIDGenerator
@@ -25,10 +29,12 @@ type ClientFactory struct {
 	logger        Logger
 }
 
+// Create returns a new client.
 func (f *ClientFactory) Create() WebsocketClient {
 	return NewClient(f.options, f.uuidGenerator.GenerateV4(), f.upgrader, f.logger)
 }
 
+// NewClientFactory initializes a new ClientFactory.
 func NewClientFactory(
 	options ClientOptions,
 	uuidGenerator UUIDGenerator,
