41e50e5a7c02fdd897e95143f7d0ef3183f807d4
Andrey Bukatov <andrey.bukatov@gmail.com>
2020-02-16 16:35:08 +0200

    Add doc for message

--- message.go
+++ message.go
@@ -1,5 +1,6 @@
 package wspubsub
 
+// MessageType enumerates possible message types.
 type MessageType byte
 
 const (
@@ -8,27 +9,33 @@ const (
 	MessageTypePing   MessageType = 9
 )
 
+// Message represents a data type to send over a WebSocket connection.
 type Message struct {
 	Type    MessageType
 	Payload []byte
 }
 
+// NewTextMessage initializes a new text Message from bytes.
 func NewTextMessage(payload []byte) Message {
 	return Message{Type: MessageTypeText, Payload: payload}
 }
 
+// NewTextMessage initializes a new text Message from string.
 func NewTextMessageFromString(payload string) Message {
 	return NewTextMessage([]byte(payload))
 }
 
+// NewBinaryMessage initializes a new binary Message from bytes.
 func NewBinaryMessage(payload []byte) Message {
 	return Message{Type: MessageTypeBinary, Payload: payload}
 }
 
+// NewBinaryMessageFromString initializes a new binary Message from string.
 func NewBinaryMessageFromString(payload string) Message {
 	return NewBinaryMessage([]byte(payload))
 }
 
+// NewPingMessage initializes a new ping Message.
 func NewPingMessage() Message {
 	return Message{Type: MessageTypePing}
 }
